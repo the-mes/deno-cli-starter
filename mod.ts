17145733601856cccb372afe313a8e8314696a16
Jakub Biesiada <kbba@op.pl>
2021-04-09 20:32:22 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@ import example from "./commands/example.ts";
 const program = new Denomander({
   app_name: "Deno CLI starter",
   app_description: "Template for Deno CLI app",
-  app_version: "1.0.0",
+  app_version: "0.0.0",
 });
 
 program
